57a8dac442a29d369c95cd8a3ef6665bd06b1293
Oleg <keeperoleg26@gmail.com>
2023-04-05 14:45:38 +0300

    added  tes  dependency

--- src/mediaPlan/handler.ts
+++ src/mediaPlan/handler.ts
@@ -1,9 +1,10 @@
 
+import moment from 'moment';
 import { config } from 'node-config-ts';
 
 console.log(config.StackEnv)
 
 module.exports.handler = () => {
-  console.log('TEST--------------------->');
-  return null;
+  console.log('TEST--------------------->',moment());
+  return 'done';
 };
